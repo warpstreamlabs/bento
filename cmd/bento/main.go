a76a245ac869b00425259b8cd2d15cacab63811a
Greg Furman <gregfurman99@gmail.com>
2024-05-23 17:17:16 +0100

    Add CLI options

--- cmd/bento/main.go
+++ cmd/bento/main.go
@@ -9,6 +9,22 @@ import (
 	_ "github.com/warpstreamlabs/bento/public/components/all"
 )
 
+var (
+	// Version version set at compile time.
+	Version string
+	// DateBuilt date built set at compile time.
+	DateBuilt string
+	// BinaryName binary name.
+	BinaryName string = "bento"
+)
+
 func main() {
-	service.RunCLI(context.Background())
+	service.RunCLI(
+		context.Background(),
+		service.CLIOptSetVersion(Version, DateBuilt),
+		service.CLIOptSetBinaryName(BinaryName),
+		service.CLIOptSetProductName("Bento"),
+		service.CLIOptSetDocumentationURL("https://warpstreamlabs.github.io/bento/docs"),
+		service.CLIOptSetShowRunCommand(true),
+	)
 }
