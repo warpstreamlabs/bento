7acd97aabb2be92130ac5fc19162d5f5f04496dc
Greg Furman <gregfurman99@gmail.com>
2024-12-07 17:51:54 +0200

    make deps and lint

--- internal/impl/sql/conn_aws.go
+++ internal/impl/sql/conn_aws.go
@@ -7,12 +7,12 @@ import (
 )
 
 const (
-	SqlFieldAWS = "aws"
+	SQLFieldAWS = "aws"
 )
 
 // AWSGetCredentialsGeneratorFn is populated with the child `aws` package when imported.
 var AWSGetCredentialsGeneratorFn = func(c *service.ParsedConfig) (fn func(dsn, driver string) (password string, err error), err error) {
-	if c.Contains(SqlFieldAWS) {
+	if c.Contains(SQLFieldAWS) {
 		return nil, errors.New("unable to configure AWS authentication as this binary does not import components/aws")
 	}
 	return
