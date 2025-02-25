3da5b1e1239fe0d7f4ccb57b6ac734a82f0dcaf4
Jem Davies <131159520+jem-davies@users.noreply.github.com>
2025-02-25 16:46:06 +0000

    Update internal/impl/aws/processor_s3.go
    Co-authored-by: Greg Furman <31275503+gregfurman@users.noreply.github.com>

--- internal/impl/aws/processor_s3.go
+++ internal/impl/aws/processor_s3.go
@@ -86,7 +86,7 @@ input:
 			Description("Forces the client API to use path style URLs for downloading keys, which is often required when connecting to custom endpoints.").
 			Default(false),
 		service.NewBoolField(s3iFieldDeleteObjects).
-			Description("Whether to delete downloaded objects from the bucket once they are processed. Note: the S3 Object will be deleted as soon as this processor has consumed the object, but the message hasn't left the Bento stream.").
+			Description("Whether to delete downloaded objects from the bucket once they are processed. Note: the S3 Object will be deleted from AWS as soon as this processor has consumed the object.").
 			Version("1.5.0").
 			Default(false).
 			Advanced(),
