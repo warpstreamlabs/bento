512d228bdbd14e6f9a645846e7c2eca6e3644007
adrian <adrian.hajduk@h2o.ai>
2025-09-19 10:21:33 +0200

    code refactoring

--- internal/impl/azure/input_service_bus_queue.go
+++ internal/impl/azure/input_service_bus_queue.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+
 	"github.com/warpstreamlabs/bento/public/service"
 )
 
