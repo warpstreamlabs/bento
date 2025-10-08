804ccf490c96e0144ec89798793676c142519706
Jem Davies <131159520+jem-davies@users.noreply.github.com>
2025-10-08 19:16:19 +0800

    Add benchmark for kafka (#460)
    * add kafka benchmark
    
    Signed-off-by: Jem Davies <jemsot@gmail.com>
    
    * add the .prof profile data
    
    Signed-off-by: Jem Davies <jemsot@gmail.com>
    
    * fix lint
    
    Signed-off-by: Jem Davies <jemsot@gmail.com>
    
    * remove .prof files & add to .gitignore
    
    Signed-off-by: Jem Davies <jemsot@gmail.com>
    
    * Update .gitignore
    
    ---------
    
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- public/service/integration/stream_benchmark_definitions.go
+++ public/service/integration/stream_benchmark_definitions.go
@@ -2,6 +2,8 @@ package integration
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 	"testing"
 
@@ -74,6 +76,79 @@ func StreamBenchSend(batchSize, parallelism int) StreamBenchDefinition {
 			close(batchChan)
 
 			wg.Wait()
+		},
+	)
+}
+
+// StreamBenchSendReportThroughput benchmarks the speed at which messages are
+// sent over the templated output and then subsequently received from the input
+// with a given; batch size, payload size & parallelism. Reports throughput in
+// MiB/s for end-to-end message processing.
+func StreamBenchSendReportThroughput(batchSize, payloadSize, parallelism int) StreamBenchDefinition {
+	return namedBench(
+		fmt.Sprintf("send message batches %v with parallelism %v ", batchSize, parallelism),
+		func(b *testing.B, env *streamTestEnvironment) {
+			require.Greater(b, parallelism, 0)
+
+			tranChan := make(chan message.Transaction, 1000)
+			input, output := initConnectors(b, tranChan, env)
+			b.Cleanup(func() {
+				closeConnectors(b, env, input, output)
+			})
+
+			sends := b.N / batchSize
+
+			set := map[string][]string{}
+			totalBytes := 0
+			for j := range sends {
+				for i := range batchSize {
+					payload := strings.Repeat("X", payloadSize) + strconv.Itoa(j*sends+i)
+					set[payload] = nil
+					totalBytes += len(payload)
+				}
+			}
+
+			b.ResetTimer()
+
+			batchChan := make(chan []string, 1000)
+
+			var wg sync.WaitGroup
+			for range parallelism {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					for {
+						batch, open := <-batchChan
+						if !open {
+							return
+						}
+						assert.NoError(b, sendBatch(env.ctx, b, tranChan, batch))
+					}
+				}()
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for len(set) > 0 {
+					messagesInSet(b, true, true, receiveBatch(env.ctx, b, input.TransactionChan(), nil), set)
+				}
+			}()
+
+			for j := range sends {
+				payloads := []string{}
+				for i := range batchSize {
+					payload := strings.Repeat("X", payloadSize) + strconv.Itoa(j*sends+i)
+					payloads = append(payloads, payload)
+				}
+				batchChan <- payloads
+			}
+			close(batchChan)
+
+			wg.Wait()
+
+			mebiBytesPerSecond := (float64(totalBytes) / (1024 * 1024)) / b.Elapsed().Seconds()
+			b.ReportMetric(mebiBytesPerSecond, "MiB/s")
 		},
 	)
 }
