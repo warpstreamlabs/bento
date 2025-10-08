867c3583eb8cd9fa98c523a30aab8db10d99c0f3
Greg Furman <31275503+gregfurman@users.noreply.github.com>
2025-10-08 12:06:48 +0200

    chore: update docs

--- internal/impl/sql/conn_fields_init.go
+++ internal/impl/sql/conn_fields_init.go
@@ -5,6 +5,5 @@ package sql
 import "github.com/warpstreamlabs/bento/public/service"
 
 func init() {
-	driverField = service.NewStringEnumField("driver", "mysql", "postgres", "clickhouse", "mssql", "sqlite", "oracle", "snowflake", "trino", "gocosmos", "spanner").
-		Description("A database [driver](#drivers) to use.").LintRule(`root = if this == "duckdb" { [ "Cannot use DuckDB driver outside of CGO build. Use a binary built with tag x_bento_extra, or the CGO Docker image" ] }`)
+	driverField = driverField.LintRule(`root = if this == "duckdb" { [ "Cannot use DuckDB driver outside of CGO build. Use a binary built with tag x_bento_extra, or the CGO Docker image" ] }`)
 }
