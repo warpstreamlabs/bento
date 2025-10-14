ff9ce04384e03f002d2b80e330819c547f8b865f
Joao Correia <joao.correia@gmail.com>
2025-10-14 10:59:11 -0700

    feat: New Opensnowcat processor (#490)

---
+++ public/components/opensnowcat/package.go
@@ -0,0 +1,7 @@
+// Package opensnowcat imports all OpenSnowcat/Snowplow component implementations.
+package opensnowcat
+
+import (
+	// Import opensnowcat components
+	_ "github.com/warpstreamlabs/bento/internal/impl/opensnowcat"
+)
