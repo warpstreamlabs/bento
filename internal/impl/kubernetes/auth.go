fae51685a800bd4c8ff46a867bc3b228b74f448b
David Aronchick <aronchick@gmail.com>
2025-12-22 19:15:07 -0800

    refactor(kubernetes): add Windows build exclusion for InClusterNamespace
    Move InClusterNamespace to auth_unix.go with Unix-only build tag since
    it reads /var/run/secrets/kubernetes.io/serviceaccount/namespace which
    doesn't exist on Windows. Add auth_windows.go stub that returns default.

--- internal/impl/kubernetes/auth.go
+++ internal/impl/kubernetes/auth.go
@@ -247,14 +247,3 @@ func buildExplicitClient(conf *service.ParsedConfig, fs *service.FS) (*rest.Conf
 
 	return config, nil
 }
-
-// InClusterNamespace returns the namespace this pod is running in,
-// or "default" if not running in a cluster.
-func InClusterNamespace() string {
-	// Try to read the namespace from the service account
-	nsBytes, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
-	if err == nil {
-		return string(nsBytes)
-	}
-	return "default"
-}
