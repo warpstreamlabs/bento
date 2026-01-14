abcfa1372b7888f1b00cd6ae259fe95cc6faf988
Greg Furman <31275503+gregfurman@users.noreply.github.com>
2026-01-14 22:14:11 +0100

    feat(core/messages): update message proto spec for multi-batch support (#635)

--- internal/message/messagepb/message.pb.go
+++ internal/message/messagepb/message.pb.go
@@ -164,6 +164,50 @@ func (x *Batch) GetParts() []*Part {
 	return nil
 }
 
+type Batches struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Batches       []*Batch               `protobuf:"bytes,1,rep,name=batches,proto3" json:"batches,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *Batches) Reset() {
+	*x = Batches{}
+	mi := &file_message_proto_msgTypes[2]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *Batches) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Batches) ProtoMessage() {}
+
+func (x *Batches) ProtoReflect() protoreflect.Message {
+	mi := &file_message_proto_msgTypes[2]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Batches.ProtoReflect.Descriptor instead.
+func (*Batches) Descriptor() ([]byte, []int) {
+	return file_message_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *Batches) GetBatches() []*Batch {
+	if x != nil {
+		return x.Batches
+	}
+	return nil
+}
+
 var File_message_proto protoreflect.FileDescriptor
 
 const file_message_proto_rawDesc = "" +
@@ -178,7 +222,9 @@ const file_message_proto_rawDesc = "" +
 	"\bmetadata\x18\x04 \x01(\v2\x17.google.protobuf.StructR\bmetadataB\t\n" +
 	"\acontent\".\n" +
 	"\x05Batch\x12%\n" +
-	"\x05parts\x18\x01 \x03(\v2\x0f.messagepb.PartR\x05partsB<Z:github.com/warpstreamlabs/bento/internal/message/messagepbb\x06proto3"
+	"\x05parts\x18\x01 \x03(\v2\x0f.messagepb.PartR\x05parts\"5\n" +
+	"\aBatches\x12*\n" +
+	"\abatches\x18\x01 \x03(\v2\x10.messagepb.BatchR\abatchesB<Z:github.com/warpstreamlabs/bento/internal/message/messagepbb\x06proto3"
 
 var (
 	file_message_proto_rawDescOnce sync.Once
@@ -192,22 +238,24 @@ func file_message_proto_rawDescGZIP() []byte {
 	return file_message_proto_rawDescData
 }
 
-var file_message_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_message_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_message_proto_goTypes = []any{
 	(*Part)(nil),            // 0: messagepb.Part
 	(*Batch)(nil),           // 1: messagepb.Batch
-	(*structpb.Value)(nil),  // 2: google.protobuf.Value
-	(*structpb.Struct)(nil), // 3: google.protobuf.Struct
+	(*Batches)(nil),         // 2: messagepb.Batches
+	(*structpb.Value)(nil),  // 3: google.protobuf.Value
+	(*structpb.Struct)(nil), // 4: google.protobuf.Struct
 }
 var file_message_proto_depIdxs = []int32{
-	2, // 0: messagepb.Part.structured:type_name -> google.protobuf.Value
-	3, // 1: messagepb.Part.metadata:type_name -> google.protobuf.Struct
+	3, // 0: messagepb.Part.structured:type_name -> google.protobuf.Value
+	4, // 1: messagepb.Part.metadata:type_name -> google.protobuf.Struct
 	0, // 2: messagepb.Batch.parts:type_name -> messagepb.Part
-	3, // [3:3] is the sub-list for method output_type
-	3, // [3:3] is the sub-list for method input_type
-	3, // [3:3] is the sub-list for extension type_name
-	3, // [3:3] is the sub-list for extension extendee
-	0, // [0:3] is the sub-list for field type_name
+	1, // 3: messagepb.Batches.batches:type_name -> messagepb.Batch
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_message_proto_init() }
@@ -225,7 +273,7 @@ func file_message_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_message_proto_rawDesc), len(file_message_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
