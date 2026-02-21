744a2ea46ca05c8d13b17eee7f51801b4fcd3a37
Antoine GIRARD <antoine.girard@sapk.fr>
2026-02-21 23:59:47 +0100

    cleanup duplicate files

--- internal/impl/sql/conn_fields_init.go
+++
@@ -1,7 +0,0 @@
-//go:build !x_bento_extra
-
-package sql
-
-func init() {
-	driverField = driverField.LintRule(`root = if this == "duckdb" { [ "Cannot use DuckDB driver outside of CGO build. Use a binary built with tag x_bento_extra, or the CGO Docker image" ] }`)
-}
