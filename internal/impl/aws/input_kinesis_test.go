88b3cde641494c3ad148db6478b13b64a490ca70
Jem Davies <jemsot@gmail.com>
2026-03-13 12:08:14 +0000

    fix lint
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- internal/impl/aws/input_kinesis_test.go
+++ internal/impl/aws/input_kinesis_test.go
@@ -115,7 +115,6 @@ func TestIsShardFinished(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		test := test
 		t.Run(test.name, func(t *testing.T) {
 			result := isShardFinished(test.shard)
 			assert.Equal(t, test.expected, result)
