3f60caab6d7c1726279300a4693af32ea4a4c972
Jem Davies <jemsot@gmail.com>
2026-04-02 15:01:52 +0100

    refactor tests to table driven tests
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- internal/impl/grpc_client/client_input_test.go
+++ internal/impl/grpc_client/client_input_test.go
@@ -15,28 +15,81 @@ import (
 )
 
 func TestGrpcClientInput(t *testing.T) {
-	testServer := startGRPCServer(t, withReflection())
-
-	config := fmt.Sprintf(`
+	tests := map[string]struct {
+		grpcServerOpts   []testServerOpt
+		confFormatString string
+		formatArgs       func(*testServer) []any
+		expectedMessage  []byte
+	}{
+		"Unary Reflection": {
+			grpcServerOpts: []testServerOpt{withReflection()},
+			confFormatString: `
 grpc_client:
   address: localhost:%v
   service: helloworld.Greeter
   method: SayHello
   rpc_type: unary
   reflection: true
-  payload: ${! {"name":"Jem"} }
-`, testServer.port)
-
-	msgCh := startGrpcClientInput(t, config)
-
-	var msg message.Transaction
-	select {
-	case msg = <-msgCh:
-	case <-time.After(time.Second * 10):
-		t.FailNow()
+  payload: ${! {"name":"Alice"} }
+`,
+			formatArgs: func(ts *testServer) []any {
+				return []any{ts.port}
+			},
+			expectedMessage: []byte(`{"message":"Hello Alice"}`),
+		},
+		"Unary Proto File": {
+			grpcServerOpts: []testServerOpt{},
+			confFormatString: `
+grpc_client:
+  address: localhost:%v
+  service: helloworld.Greeter
+  method: SayHello
+  rpc_type: unary
+  proto_files: 
+    - "./grpc_test_server/helloworld.proto"
+  payload: ${! {"name":"Alice"} }
+`,
+			formatArgs: func(ts *testServer) []any {
+				return []any{ts.port}
+			},
+			expectedMessage: []byte(`{"message":"Hello Alice"}`),
+		},
 	}
 
-	assert.Equal(t, "{\"message\":\"Hello Jem\"}", string(msg.Payload.Get(0).AsBytes()))
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			testServer := startGRPCServer(t, test.grpcServerOpts...)
+
+			yamlConf := fmt.Sprintf(test.confFormatString, test.formatArgs(testServer)...)
+
+			msgCh := startGrpcClientInput(t, yamlConf)
+
+			var msg message.Transaction
+			select {
+			case msg = <-msgCh:
+			case <-time.After(time.Second * 10):
+				t.FailNow()
+			}
+
+			assert.Equal(t, test.expectedMessage, msg.Payload.Get(0).AsBytes())
+		})
+	}
+}
+
+func startGrpcClientInput(t *testing.T, yamlConf string) (ch <-chan (message.Transaction)) {
+	t.Helper()
+
+	conf, err := testutil.InputFromYAML(yamlConf)
+	require.NoError(t, err)
+
+	s, err := mock.NewManager().NewInput(conf)
+	require.NoError(t, err)
+
+	t.Cleanup(s.TriggerCloseNow)
+
+	return s.TransactionChan()
 }
 
 func TestGrpcClientInputRateLimit(t *testing.T) {
@@ -52,7 +105,7 @@ input:
     method: SayHello
     rpc_type: unary
     reflection: true
-    payload: ${! {"name":"Jem"} }
+    payload: ${! {"name":"Alice"} }
     rate_limit: basic
 
 rate_limit_resources:
@@ -89,50 +142,10 @@ rate_limit_resources:
 		select {
 		case <-time.After(time.Second * 2):
 			assert.Equal(t, 1, len(msgReceived))
-			assert.Equal(t, `{"message":"Hello Jem"}`, string(msgReceived[0]))
+			assert.Equal(t, `{"message":"Hello Alice"}`, string(msgReceived[0]))
 			return
 		case msg := <-ch:
 			msgReceived = append(msgReceived, msg)
 		}
 	}
-}
-
-func TestGrpcClientInputProtoFile(t *testing.T) {
-	testServer := startGRPCServer(t, withReflection())
-
-	config := fmt.Sprintf(`
-grpc_client:
-  address: localhost:%v
-  service: helloworld.Greeter
-  method: SayHello
-  rpc_type: unary
-  proto_files: 
-    - "./grpc_test_server/helloworld.proto"
-  payload: ${! {"name":"Jem"} }
-`, testServer.port)
-
-	msgCh := startGrpcClientInput(t, config)
-
-	var msg message.Transaction
-	select {
-	case msg = <-msgCh:
-	case <-time.After(time.Second * 10):
-		t.FailNow()
-	}
-
-	assert.Equal(t, "{\"message\":\"Hello Jem\"}", string(msg.Payload.Get(0).AsBytes()))
-}
-
-func startGrpcClientInput(t *testing.T, yamlConf string) (ch <-chan (message.Transaction)) {
-	t.Helper()
-
-	conf, err := testutil.InputFromYAML(yamlConf)
-	require.NoError(t, err)
-
-	s, err := mock.NewManager().NewInput(conf)
-	require.NoError(t, err)
-
-	t.Cleanup(s.TriggerCloseNow)
-
-	return s.TransactionChan()
 }
