ad6c8d4360005a622375c5d3f7038efba0518e33
Jem Davies <131159520+jem-davies@users.noreply.github.com>
2026-06-11 17:10:36 +0100

    Update internal/impl/grpc/input_grpc_client.go
    Co-authored-by: Greg Furman <31275503+gregfurman@users.noreply.github.com>

--- internal/impl/grpc/input_grpc_client.go
+++ internal/impl/grpc/input_grpc_client.go
@@ -25,8 +25,8 @@ Either the field ` + "`reflection` or `proto_files`" + ` must be supplied, which
 `
 
 const grpcClientInputPayloadDescription = `
-For ` + "`rpc_type`" + ` values: unary, client_stream & server_stream, the payload field defines the data that is sent as the request.
-In the instance of the client_stream rpc_type - this is expected to resolve to an array. The array will be sent as a stream, with the elements
+For ` + "`rpc_type`" + ` values: ` + "`unary`" + `, ` + "`client_stream`" + ` & ` + "`server_stream`" + `, the ` + "`payload`" + ` field defines the data that is sent as the request.
+In the instance of the ` + "`client_stream`" + ` rpc_type - this is expected to resolve to an array. The array will be sent as a stream, with the elements
 making the individual messages.
 `
 
