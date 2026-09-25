3dcef2a11e2e7c1516c18c07e0387e2721518528
Jem Davies <jemsot@gmail.com>
2026-09-25 13:21:58 +0100

    adds doc string for FieldWithAlias func
    Signed-off-by: Jem Davies <jemsot@gmail.com>

--- public/service/config.go
+++ public/service/config.go
@@ -749,7 +749,14 @@ func (p *ParsedConfig) FieldObjectMap(path ...string) (map[string]*ParsedConfig,
 	return pl, nil
 }
 
-// TODO
+// FieldWithAlias returns the value of the first path in paths that is present in
+// the parsed config, reading it with get. This is intended for fields that have
+// Aliases.
+//
+// Paths are checked in order and the first match wins; later paths are not consulted
+// and no error is returned if serveral are set. Use the .Alias() builder function
+// on the ConfigSpec, such that if multiple fields that match the Alias are provided
+// a linter error is thrown.
 func FieldWithAlias[T any](p *ParsedConfig, get func(...string) (T, error), paths ...[]string) (T, error) {
 	for _, path := range paths {
 		if p.Contains(path...) {
